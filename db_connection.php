19fb2a9009d9b722551e15feec1cca6a1bff9ffc
Jonas de Bruin <jonasdebruin05@gmail.com>
2023-09-09 14:00:14 +0200

    Added check if fields are filled in

--- db_connection.php
+++ db_connection.php
@@ -7,10 +7,21 @@ $dbname = "logintest";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-echo "Hello World!";
 // Check connection
-if (!$conn) {
+if ($conn) {
+  echo "Connected successfully";
+}
+else {
   echo "Connection failed: ";
 }
-echo "Connected successfully";
+
+//check if the field with id uName and uPass is set and not empty, print out succes if true, else print out failure
+
+
+if(isset($_POST['uName']) && !empty($_POST['uName']) && isset($_POST['uPass']) && !empty($_POST['uPass'])) {
+  echo "Success!";
+} 
+else {
+  echo "Failure!";
+}
 ?>
